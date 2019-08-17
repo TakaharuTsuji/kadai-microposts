a7732f13828a2ecc5ca8edf6f6a4b24b5d4ac02d
momonga.kapibara <silvermomonga@outlook.com>
2019-08-17 13:13:22 +0000

    added like to navber

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -4,13 +4,13 @@ class FavoritesController < ApplicationController
     likempost = Micropost.find(params[:like_id])
     current_user.favorite(likempost)
     flash[:success] = 'お気に入りに追加しました。'
-    redirect_to "/"
+    redirect_to likes_user_path(current_user)
   end
 
   def destroy
     unlikempost = Micropost.find(params[:like_id])
     current_user.unfavorite(unlikempost)
     flash[:success] = 'お気に入りを解除しました。'
-    redirect_to  "/"
+    redirect_to likes_user_path(current_user)
   end
 end
