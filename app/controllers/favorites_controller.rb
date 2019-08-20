a15e8b1d6e2a2010e90cbab56db3b603526e959d
momonga.kapibara <silvermomonga@outlook.com>
2019-08-20 02:05:57 +0000

    moded view of top,likes and cont of favo

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -4,14 +4,14 @@ class FavoritesController < ApplicationController
     likempost = Micropost.find(params[:like_id])
     current_user.favorite(likempost)
     flash[:success] = 'お気に入りに追加しました。'
-    redirect_to likes_user_path(current_user)
+    redirect_back(fallback_location: root_path)
   end
 
   def destroy
     unlikempost = Micropost.find(params[:like_id])
     current_user.unfavorite(unlikempost)
     flash[:success] = 'お気に入りを解除しました。'
-    redirect_to likes_user_path(current_user)
+    redirect_back(fallback_location: root_path)
   end
 
 end
